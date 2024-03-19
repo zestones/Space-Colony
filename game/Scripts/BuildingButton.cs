15ce56c03376173379f7dfa3777c9c7cdc4ceb34
Sqaishy <JakeEbudd@icloud.com>
2024-03-19 14:59:07 +0000

    Buttons Now Reflect Names
    Material and building buttons now display the names correctly.

--- game/Scripts/BuildingButton.cs
+++ game/Scripts/BuildingButton.cs
@@ -7,6 +7,11 @@ public partial class BuildingButton : Button
 	[Export] public BuildingResource building;
 	[Export] public Inventory inventory;
 
+	public override void _Ready()
+	{
+		Text = building.buildingName;
+	}
+
 	public override void _Pressed()
 	{
 		foreach (KeyValuePair<MaterialResource, int> pair in building.buildingMaterials)
--- game/Scripts/DebugAddMaterial.cs
+++ game/Scripts/DebugAddMaterial.cs
@@ -6,6 +6,11 @@ public partial class DebugAddMaterial : Button
 	[Export] public int amount;
 	[Export] public Inventory inventory;
 
+	public override void _Ready()
+	{
+		Text = material.materialName;
+	}
+
 	public override void _Pressed()
 	{
 		inventory.AddMaterial(material.materialName, amount);

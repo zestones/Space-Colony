71fe1049fb31605e3668b33864dfb697df694de1
pressgoal <isaiahmnasby@gmail.com>
2024-03-24 22:16:33 -0500

    Spaceship can now move towards the cursor
    With the targetMouse variable, the spaceship can now fly towards the cursor.
    
    Aside for that, I also updated the bullet code to use @exports instead of metadata, and I drastically increased the bullet speed.

--- game/src/Player/Spaceship/Bullet/bullet.gd
+++ game/src/Player/Spaceship/Bullet/bullet.gd
@@ -1,14 +1,14 @@
 extends Area2D
 
 var lifespan = 10;
+@export var speed : int = 1000;
 
 func _ready():
-	if get_meta("targetCursor"):
-		rotation = atan2(get_local_mouse_position().y, get_local_mouse_position().x);
+	rotation = atan2(get_local_mouse_position().y, get_local_mouse_position().x);
 
 func _process(delta):
 	var direction = Vector2(cos(rotation), sin(rotation));
-	position += direction * get_meta("speed") * delta;
+	position += direction * speed * delta;
 	lifespan -= delta;
 	
 	if lifespan <= 0:
--- game/src/Player/Spaceship/spaceshipRevised.gd
+++ game/src/Player/Spaceship/spaceshipRevised.gd
@@ -2,23 +2,36 @@ extends CharacterBody2D
 
 var bulletID = preload ("res://src/Player/Spaceship/Bullet/bullet.tscn");
 @onready var anim = get_node("AnimatedSprite2D");
+## The maximum speed that the spaceship can move.
 @export var maxSpeed : int = 200;
+## The amount that the spaceship will speed up by in one second.
 @export var acceleration : int = 100;
+## How fast the spaceship adjusts its angle (in degrees).
 @export var rotationalSpeed : int = 360;
+## The amount that the spaceship will slow down by in one second when not given any input.
 @export var inertia : int = 200;
+## Whether or not the spaceship flies towards the cursor.
+@export var targetMouse : bool = true;
 
 var speed = 0;
 var direction = Vector2.ZERO;
 
 func _process(delta):
 	var input = Input.get_vector("Left", "Right", "Up", "Down").normalized();
-	if input != Vector2.ZERO:
+	if targetMouse:
+		direction = get_local_mouse_position().normalized();
+		anim.rotation = direction.angle();
+		speed += acceleration * delta * max(0, -input.y);
+		speed = clampi(speed, 0, maxSpeed);
+		if input.y >= 0:
+			speed = move_toward(speed, 0, inertia * delta);
+	elif input != Vector2.ZERO:
 		direction = input;
-		anim.rotation = rotate_toward(anim.rotation, input.angle(), deg_to_rad(rotationalSpeed) * delta);
 		speed += acceleration * delta;
 		speed = clampi(speed, 0, maxSpeed);
 	else:
 		speed = move_toward(speed, 0, inertia * delta);
+	anim.rotation = rotate_toward(anim.rotation, direction.angle(), deg_to_rad(rotationalSpeed) * delta);
 	
 	velocity = direction * speed;
 	
@@ -31,7 +44,7 @@ func _process(delta):
 	animate(input);
 
 func animate(input):
-	if input == Vector2.ZERO:
+	if input == Vector2.ZERO || (targetMouse && input.y >= 0):
 		anim.play("Idle");
 	else:
 		anim.play("Moving");

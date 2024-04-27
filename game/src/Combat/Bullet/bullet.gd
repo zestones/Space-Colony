a706d8a3f5ff64582362f3a4eed4a4265df2daa9
CodeGuy-Manav <107916259+CodeGuy-Manav@users.noreply.github.com>
2024-04-27 21:15:14 +0530

    Added Player Score [Kill / Attack points

--- game/src/Combat/Bullet/bullet.gd
+++ game/src/Combat/Bullet/bullet.gd
@@ -4,6 +4,7 @@ var lifespan = 10;
 @export var speed : int = 600;
 var target : Vector2
 var Dammage : int = 50
+var Owner
 func _ready():
 	target = to_local(target)
 	rotation = atan2(target.y,target.x)
@@ -20,6 +21,9 @@ func _process(delta):
 
 func _on_body_entered(body):
 	if body.is_in_group("Alien"):
+		if Owner != null :
+			if Owner.is_in_group("Player"):
+				Owner.KillScore += 0.5
 		body.Hp -= Dammage
 		body.animator.play("Hurt")
 		$Hit.play()
